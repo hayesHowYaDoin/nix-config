23dc1fbf85a6af68f2336ea5226382bfd285f9d7
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-01-05 19:31:48 -0700

    Assign necessary sessionVariables

--- home/home.nix
+++ home/home.nix
@@ -6,17 +6,8 @@
     ./modules/desktop
   ];
 
-  home.username = user.name;
-  home.homeDirectory = user.home;
-
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   # Enable if not using NixOS
   # targets.genericLinux.enable = true;
-
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
 
   features = {
     cli = {
@@ -28,7 +19,7 @@
     };
     desktop = {
       fonts.enable = true;
-      # hyprland.enable = true;
+      hyprland.enable = true;
       wayland.enable = true;
       xdg.enable = true;
     };
@@ -36,6 +27,45 @@
       # gtk.enable = true;
       # qt.enable = true;
     # };
+  };
+
+  # Just a lil' test
+  programs.bash.enable = true;
+
+  home = {
+    username = user.name;
+    homeDirectory = user.home;
+    sessionVariables = {
+      BROWSER = "firefox";
+      EDITOR = "vim";
+      TERMINAL = "kitty";
+      NIXOS_OZONE_WL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+      SDL_VIDEODRIVER = "wayland";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      ELECTRON_EXTRA_FLAGS = "--force-device-scale-factor=1.5";
+      #_JAVA_AWT_WM_NONREPARENTING = "1";
+      #MOZ_DRM_DEVICE = "/dev/dri/card0:/dev/dri/card1";
+      #WLR_DRM_DEVICES = "/dev/dri/card0:/dev/dri/card1";
+      #WLR_NO_HARDWARE_CURSORS = "1"; # if no cursor,uncomment this line
+      #GBM_BACKEND = "nvidia-drm";
+      #CLUTTER_BACKEND = "wayland";
+      LIBVA_DRIVER_NAME = "iHD";
+      #WLR_RENDERER = "vulkan";
+      #VK_DRIVER_FILES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
+      #__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      #__NV_PRIME_RENDER_OFFLOAD = "1";
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      GTK_USE_PORTAL = "1";
+      GTK_THEME = "Nightfox-dark";
+      XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
+      XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
+      XDG_BIN_HOME = "${config.home.homeDirectory}/.nix-profile/bin";
+      XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
+    };
+    stateVersion = "24.11"; # Please read the comment before changing.
   };
 
   programs.home-manager.enable = true;
--- home/modules/desktop/hyprland.nix
+++ home/modules/desktop/hyprland.nix
@@ -1,183 +1,184 @@
-{ pkgs, lib, config, ... }:
+{ config, lib, pkgs, ...}:
 
-let
-  startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
-    ${pkgs.waybar}/bin/waybar &
-    ${pkgs.swww}/bin/swww init &
-
-    sleep 1
-
-    ${pkgs.swww}/bin/swww img ${../../assets/wallpaper.jpg} &
-  '';
+with lib; let
+  cfg = config.features.desktop.hyprland;
 in {
-  wayland.windowManager.hyprland = {
-    # Allow home-manager to configure hyprland
-    enable = true;
+  options.features.desktop.hyprland.enable = mkEnableOption "hyprland config";
 
-    settings = {
-      # Autostart
-      exec-once = ''${startupScript}/bin/start'';
+  config = mkIf cfg.enable {
+    wayland.windowManager.hyprland = {
+      enable = true;
+      settings = {
+        xwayland = {
+          force_zero_scaling = true;
+        };
+        
+        # Autostart
+        exec-once = [
+          "waybar"
+        ];
 
-      # Programs
-      "$terminal" = "kitty";
-      "$fileManager" = "dolphin";
-      "$menu" = "rofi -show drun";
+        # Programs
+        "$terminal" = "kitty";
+        "$fileManager" = "dolphin";
+        "$menu" = "rofi -show drun";
 
-      # Modifier keys
-      "$mainMod" = "SUPER";
+        # Modifier keys
+        "$mainMod" = "SUPER";
 
-      ### WINDOW ##############################################################
+        ### WINDOW ##############################################################
 
-      general  = {
-        # See https://wiki.hyprland.org/Configuring/Variables/ for more
-        gaps_in = 5;
-        gaps_out = 0;
-        border_size = 2;
-        "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-        "col.inactive_border" = "rgba(595959aa)";
+        general  = {
+          # See https://wiki.hyprland.org/Configuring/Variables/ for more
+          gaps_in = 5;
+          gaps_out = 0;
+          border_size = 2;
+          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+          "col.inactive_border" = "rgba(595959aa)";
 
-        layout = "dwindle";
-      };
-
-      decoration = {
-        rounding = 5;
-
-        # Applies universally; see windowrulev2 for alternative
-        # active_opacity = "0.95";
-        # inactive_opacity = "0.95";
-
-        shadow = {
-          enabled = true;
-          range = 4;
-          render_power = 3;
-          color = "rgba(ffffffff)";
+          layout = "dwindle";
         };
 
-        blur = {
-          enabled = true;
-          size = 3;
-          passes = 1;
-          vibrancy = "0.1696";
+        decoration = {
+          rounding = 5;
+
+          # Applies universally; see windowrulev2 for alternative
+          # active_opacity = "0.95";
+          # inactive_opacity = "0.95";
+
+          shadow = {
+            enabled = true;
+            range = 4;
+            render_power = 3;
+            color = "rgba(ffffffff)";
+          };
+
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            vibrancy = "0.1696";
+          };
         };
+
+        windowrulev2 = [
+          "float,class:^(Lxappearance)$"
+          "opacity 0.8 0.8,class:^(kitty)$"
+          "opacity 1 1,class:^(firefox)"
+          "noborder,fullscreen:1"
+        ];
+        # windowrulev2 = float,class:^(Lxappearance)$
+        # windowrulev2 = opacity 0.8 0.8,title:^(rofi)(.*)$
+        # windowrulev2 = opacity 0.8 0.8,class:^(kitty)$
+        # windowrulev2 = opacity 0.8 0.8,class:^(wofi)$
+        # windowrulev2 = opacity 0.8 0.8,class:^(thunar)$
+        # windowrulev2 = maximize,class:^(winbox.exe)$
+        # # windowrulev2 = maximize,class:^(chromium)$
+        # # windowrulev2 = noanim,class:^(kitty)$
+        # windowrulev2 = maximize,title:^(nvim)$
+        # windowrulev2 = float,class:^(org.telegram.desktop|vlc)$
+        # windowrulev2 = float,title:^(ranger)$
+        # windowrulev2 = size 60% 80%,class:^(org.telegram.desktop|vlc)$
+        # windowrulev2 = size 60% 80%,title:^(Open Files|ranger)$
+        # windowrulev2 = center,class:^(org.telegram.desktop|Open Files|ranger|vlc)$
+        # windowrulev2 = opacity 0.8 0.8,title:^(Open Files|ranger)$
+        # windowrulev2 = opacity 1 1,class:^(kitty)$,title:^(nvim)(.*)$ # disable opacity while opening neovim
+        # # windowrulev2 = bordercolor rgb(000000) rgb(000000),fullscreen:1
+        # windowrulev2 = noborder,fullscreen:1 # remove border on fullscreen
+
+        ### KEYBINDS ############################################################
+
+        bind = [
+          # General
+          "$mainMod, Q, exec, $terminal"
+          "$mainMod, C, killactive"
+          "$mainMod, S, exec, $fileManager"
+          "$mainMod, TAB, exec, $menu"
+          "$mainMod, J, togglesplit"
+
+          # Focus
+          "$mainMod, left, movefocus, l"
+          "$mainMod, right, movefocus, r"
+          "$mainMod, up, movefocus, u"
+          "$mainMod, down, movefocus, d"
+
+          # Switch workspace
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
+
+          "$mainMod, KP_End, workspace, 1"
+          "$mainMod, KP_Down, workspace, 2"
+          "$mainMod, KP_Next, workspace, 3"
+          "$mainMod, KP_Left, workspace, 4"
+          "$mainMod, KP_Begin, workspace, 5"
+          "$mainMod, KP_Right, workspace, 6"
+          "$mainMod, KP_Home, workspace, 7"
+          "$mainMod, KP_Up, workspace, 8"
+          "$mainMod, KP_Prior, workspace, 9"
+          "$mainMod, KP_Insert, workspace, 10"
+
+          "$mainMod SHIFT, right, workspace, e+1"
+          "$mainMod SHIFT, left, workspace, e-1"
+
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
+
+          # Move active window to workspace
+          "$mainMod SHIFT, 1, movetoworkspace, 1"
+          "$mainMod SHIFT, 2, movetoworkspace, 2"
+          "$mainMod SHIFT, 3, movetoworkspace, 3"
+          "$mainMod SHIFT, 4, movetoworkspace, 4"
+          "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+          "$mainMod SHIFT, KP_End, movetoworkspace, 1"
+          "$mainMod SHIFT, KP_Down, movetoworkspace, 2"
+          "$mainMod SHIFT, KP_Next, movetoworkspace, 3"
+          "$mainMod SHIFT, KP_Left, movetoworkspace, 4"
+          "$mainMod SHIFT, KP_Begin, movetoworkspace, 5"
+          "$mainMod SHIFT, KP_Right, movetoworkspace, 6"
+          "$mainMod SHIFT, KP_Home, movetoworkspace, 7"
+          "$mainMod SHIFT, KP_Up, movetoworkspace, 8"
+          "$mainMod SHIFT, KP_Prior, movetoworkspace, 9"
+          "$mainMod SHIFT, KP_Insert, movetoworkspace, 10"
+        ];
+
+        bindm = [
+          # Move window
+          "$mainMod, mouse:272, movewindow"
+          "$mainMod, mouse:273, movewindow"
+        ];
+
+        bindel = [
+          # Multimedia keys
+          ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+          ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+          ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+          ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+          ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
+          ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
+        ];
+
+        bindl = [
+          # Requires playerctl
+          ", XF86AudioNext, exec, playerctl next"
+          ", XF86AudioPause, exec, playerctl play-pause"
+          ", XF86AudioPlay, exec, playerctl play-pause"
+          ", XF86AudioPrev, exec, playerctl previous"
+        ];
       };
-
-      windowrulev2 = [
-        "float,class:^(Lxappearance)$"
-        "opacity 0.8 0.8,class:^(kitty)$"
-        "opacity 1 1,class:^(firefox)"
-        "noborder,fullscreen:1"
-      ];
-      # windowrulev2 = float,class:^(Lxappearance)$
-      # windowrulev2 = opacity 0.8 0.8,title:^(rofi)(.*)$
-      # windowrulev2 = opacity 0.8 0.8,class:^(kitty)$
-      # windowrulev2 = opacity 0.8 0.8,class:^(wofi)$
-      # windowrulev2 = opacity 0.8 0.8,class:^(thunar)$
-      # windowrulev2 = maximize,class:^(winbox.exe)$
-      # # windowrulev2 = maximize,class:^(chromium)$
-      # # windowrulev2 = noanim,class:^(kitty)$
-      # windowrulev2 = maximize,title:^(nvim)$
-      # windowrulev2 = float,class:^(org.telegram.desktop|vlc)$
-      # windowrulev2 = float,title:^(ranger)$
-      # windowrulev2 = size 60% 80%,class:^(org.telegram.desktop|vlc)$
-      # windowrulev2 = size 60% 80%,title:^(Open Files|ranger)$
-      # windowrulev2 = center,class:^(org.telegram.desktop|Open Files|ranger|vlc)$
-      # windowrulev2 = opacity 0.8 0.8,title:^(Open Files|ranger)$
-      # windowrulev2 = opacity 1 1,class:^(kitty)$,title:^(nvim)(.*)$ # disable opacity while opening neovim
-      # # windowrulev2 = bordercolor rgb(000000) rgb(000000),fullscreen:1
-      # windowrulev2 = noborder,fullscreen:1 # remove border on fullscreen
-
-      ### KEYBINDS ############################################################
-
-      bind = [
-        # General
-        "$mainMod, Q, exec, $terminal"
-        "$mainMod, C, killactive"
-        "$mainMod, S, exec, $fileManager"
-        "$mainMod, TAB, exec, $menu"
-        "$mainMod, J, togglesplit"
-
-        # Focus
-        "$mainMod, left, movefocus, l"
-        "$mainMod, right, movefocus, r"
-        "$mainMod, up, movefocus, u"
-        "$mainMod, down, movefocus, d"
-
-        # Switch workspace
-        "$mainMod, 1, workspace, 1"
-        "$mainMod, 2, workspace, 2"
-        "$mainMod, 3, workspace, 3"
-        "$mainMod, 4, workspace, 4"
-        "$mainMod, 5, workspace, 5"
-        "$mainMod, 6, workspace, 6"
-        "$mainMod, 7, workspace, 7"
-        "$mainMod, 8, workspace, 8"
-        "$mainMod, 9, workspace, 9"
-        "$mainMod, 0, workspace, 10"
-
-        "$mainMod, KP_End, workspace, 1"
-        "$mainMod, KP_Down, workspace, 2"
-        "$mainMod, KP_Next, workspace, 3"
-        "$mainMod, KP_Left, workspace, 4"
-        "$mainMod, KP_Begin, workspace, 5"
-        "$mainMod, KP_Right, workspace, 6"
-        "$mainMod, KP_Home, workspace, 7"
-        "$mainMod, KP_Up, workspace, 8"
-        "$mainMod, KP_Prior, workspace, 9"
-        "$mainMod, KP_Insert, workspace, 10"
-
-        "$mainMod SHIFT, right, workspace, e+1"
-        "$mainMod SHIFT, left, workspace, e-1"
-
-        "$mainMod, mouse_down, workspace, e+1"
-        "$mainMod, mouse_up, workspace, e-1"
-
-        # Move active window to workspace
-        "$mainMod SHIFT, 1, movetoworkspace, 1"
-        "$mainMod SHIFT, 2, movetoworkspace, 2"
-        "$mainMod SHIFT, 3, movetoworkspace, 3"
-        "$mainMod SHIFT, 4, movetoworkspace, 4"
-        "$mainMod SHIFT, 5, movetoworkspace, 5"
-        "$mainMod SHIFT, 6, movetoworkspace, 6"
-        "$mainMod SHIFT, 7, movetoworkspace, 7"
-        "$mainMod SHIFT, 8, movetoworkspace, 8"
-        "$mainMod SHIFT, 9, movetoworkspace, 9"
-        "$mainMod SHIFT, 0, movetoworkspace, 10"
-
-        "$mainMod SHIFT, KP_End, movetoworkspace, 1"
-        "$mainMod SHIFT, KP_Down, movetoworkspace, 2"
-        "$mainMod SHIFT, KP_Next, movetoworkspace, 3"
-        "$mainMod SHIFT, KP_Left, movetoworkspace, 4"
-        "$mainMod SHIFT, KP_Begin, movetoworkspace, 5"
-        "$mainMod SHIFT, KP_Right, movetoworkspace, 6"
-        "$mainMod SHIFT, KP_Home, movetoworkspace, 7"
-        "$mainMod SHIFT, KP_Up, movetoworkspace, 8"
-        "$mainMod SHIFT, KP_Prior, movetoworkspace, 9"
-        "$mainMod SHIFT, KP_Insert, movetoworkspace, 10"
-      ];
-
-      bindm = [
-         # Move window
-        "$mainMod, mouse:272, movewindow"
-        "$mainMod, mouse:273, movewindow"
-      ];
-
-      bindel = [
-        # Multimedia keys
-        ",XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
-        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
-        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
-        ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
-        ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
-      ];
-
-      bindl = [
-        # Requires playerctl
-        ", XF86AudioNext, exec, playerctl next"
-        ", XF86AudioPause, exec, playerctl play-pause"
-        ", XF86AudioPlay, exec, playerctl play-pause"
-        ", XF86AudioPrev, exec, playerctl previous"
-      ];
     };
   };
 }
