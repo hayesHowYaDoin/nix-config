2d75631eb275a816160da8a7bc9627fd8d48352a
hayesHowYaDoin <jordanhayes98@gmail.com>
2025-01-07 17:10:04 -0700

    Added p10k with pure configuration to zsh

--- flake.nix
+++ flake.nix
@@ -3,17 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-colors.url = "github:misterio77/nix-colors";
     hyprland.url = "github:hyprwm/Hyprland";
     hyprland-plugin = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
+
+    nix-colors.url = "github:misterio77/nix-colors";
   };
 
   outputs = {
--- home/modules/cli/zsh.nix
+++ home/modules/cli/zsh.nix
@@ -1,13 +1,32 @@
-{ config, lib, user, ...}:
+{ config, lib, pkgs, user, ...}:
 
 with lib; let
   cfg = config.features.cli.zsh;
+  p10k_path = ../../assets/p10k.zsh;
 in {
   options.features.cli.zsh.enable = mkEnableOption "enable extended zsh configuration";
 
   config = mkIf cfg.enable {
     programs.zsh = {
       enable = true;
+
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      initExtra = ''
+        [[ ! -f ${p10k_path} ]] || source ${p10k_path}
+      '';
+
+      zplug = {
+        enable = true;
+        plugins = [
+          {
+            name = "romkatv/powerlevel10k";
+            tags = [ "as:theme" "depth:1" ];
+          }
+          # { name = "loiccoyle/zsh-github-copilot"; }
+        ];
+      };
 
       shellAliases = {
         rebuild = "sudo nixos-rebuild switch --flake";
